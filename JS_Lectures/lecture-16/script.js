b2b73f6378a5de96d80239c08287f0a38c6ab4fc
parth-kachariya <parthkachariya668@gmail.com>
2025-09-10 21:02:59 +0530

    update || 10-09

---
+++ JS_Lectures/lecture-16/script.js
@@ -0,0 +1,46 @@
+
+/* Javascript Function */
+
+{
+  function language(name){
+    console.log("Hello " + name);
+  }
+
+  language("css")
+  language("tailwind")
+}
+
+// {
+//   function calc(a , b){
+//     return a * b
+//   }
+
+//   let ans1 = calc(5 , 4)
+//   let ans2 = calc(5 , 10)
+
+//   console.log(ans1);
+//   console.log(ans2);
+  
+// }
+
+{
+  const calc = (a , b) => {
+    return a  + b
+  }
+
+  console.log(calc(20 , 50));
+  
+}
+
+{
+  const print = name => {console.log(name)}
+
+  print("banana")
+}
+
+{
+  const print = name => console.log(name)
+  print("mangoes")
+  
+}
+
