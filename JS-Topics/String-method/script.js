4791e826b6fd874faa26952b65f85fd008447ff4
parth-kachariya <parthkachariya668@gmail.com>
2025-09-23 23:12:12 +0530

    Update || 23-09

--- JS-Topics/String-method/script.js
+++ JS-Topics/String-method/script.js
@@ -134,14 +134,104 @@ console.log("---------------------------------");
   let str = "kachariya";
   console.log(str.substring(1, 5));
   console.log("---------------------------------");
-
 }
 /* 14. slice(start, end) 
   Use: Extracts part of a string and returns it. */
 {
-    let str = "parth Kachariya";
-    console.log(str.slice(0, 8));
-    console.log(str.slice(-9));
+  let str = "parth Kachariya";
+  console.log(str.slice(0, 8));
+  console.log(str.slice(-9));
   console.log("---------------------------------");
+}
+/*15. replaceAll(searchValue, replaceValue)
+Use: Replaces all matches of regex/string */
+{
+  let str = "I think Ruth's Dog is cuter than your Dog!";
+  let regex = /Dog/g;
 
+  let result = str.replaceAll(regex, "cat");
+  console.log(result);
+  console.log("---------------------------------");
+}
+/* 16.  replace(searchValue, replaceValue) 
+use:Replaces part of the string matched by regex*/
+{
+  let str = "I think Ruth's Dog is cuter than your Dog!";
+  let regex = /Dog/i;
+  let result = str.replace(regex, "cat");
+  console.log(result);
+  console.log("---------------------------------");
+}
+/* 17. at(index) 
+   Use: Returns the character at the given index (supports negative index). */
+{
+    let str = "parth Kachariya";
+    console.log(str.at(3));
+    console.log(str.at(-1));
+    console.log("---------------------------------");
+}
+/* 18. charAt(index) 
+   use: Returns the character at the given index (does not support negative index). */
+{
+    let str = "parth Kachariya";
+    console.log(str.charAt(3));
+    console.log(str.charAt(10));
+}
+/* 19. charCodeAt(index) 
+  use Returns the UTF-16 code of the character at the given index. */
+{
+    let str = "parth Kachariya";
+    console.log(str.charCodeAt(0));
+    console.log(str.charCodeAt(7));
+    console.log("---------------------------------");
+}
+/* 20. concat(str1, str2, ...) 
+   Use: Joins two or more strings into one. */
+{
+    let str1 = "Parth";
+    let str2 = "Kachariya";
+    console.log(str1.concat(" ", str1));
+    console.log(str1.concat(" ", str2));
+    console.log("---------------------------------");
+}
+/* 21. includes(searchString, position) 
+   Use: Checks if the string contains the given substring. */
+{
+    let str = "parth Kachariya";
+    console.log(str.includes("pa"));
+    console.log(str.includes("Kachariya", 6));
+    console.log(str.includes("Kachariya", 10));
+    console.log("---------------------------------");
+}
+/* 22. endsWith(searchString, endPosition) 
+   Use: Checks if the string ends with the given substring. */
+{
+    let str = "parth Kachariya";
+    console.log(str.endsWith("Kachariya"));
+    console.log(str.endsWith("parth", 5));
+    console.log(str.endsWith("parth", 4));
+    console.log("---------------------------------");
+}
+/* 23. indexOf(searchString, position) 
+   Use: Returns the first index of the given substring, or -1 if not found. */
+{
+    let str = "parth Kachariya";
+    console.log(str.indexOf("a"));
+    console.log(str.indexOf("p",5 ));
+    console.log("---------------------------------");
+}
+
+/* 24. padStart(targetLength, padString) 
+   Use: Pads string at the beginning to reach the target length. */
+{
+    let str = "parth";
+    console.log(str.padStart(15, "_-_-"));
+    console.log("---------------------------------");
+}
+/* 25. padEnd(targetLength, padString) 
+   Use: Pads string at the end to reach the target length. */
+{
+    let str = "parth";
+    console.log(str.padEnd(15, "_-_-"));
+    console.log("---------------------------------");
 }
--- Javascript_Lab_Project/Q-50/script.js
+++ Javascript_Lab_Project/Q-50/script.js
@@ -1,7 +1,22 @@
 // 50) JavaScript Program to Create Multiline Strings
 {
-  let str =`This is a
+  let str = `This is a
   multiline
   string example.`;
   console.log(str);
 }
+
+// {
+//   let j = [10, 20, 30];
+
+//   console.log(j.join("+"));
+// }
+// {
+//   let numbers = [2, 10, 14, 17, 10];
+//   console.log(numbers.toString());
+// }
+// {
+//   let a = 10;
+//    let b = '10';
+//    console.log( a !== b && a != b );
+// }
