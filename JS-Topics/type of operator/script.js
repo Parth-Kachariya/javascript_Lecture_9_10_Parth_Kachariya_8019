31f77e871de8e676bf58f98f4ed92e5ff8bc6116
parth-kachariya <parthkachariya668@gmail.com>
2025-10-11 08:27:06 +0530

    11-10

---
+++ JS-Topics/type of operator/script.js
@@ -0,0 +1,58 @@
+// type of operator in js
+{
+  // 1) arithmetic oparator
+      // - addition (+)
+      // - subtraction (-)
+      // - multiplication (*)
+      // - divison (/)
+      // - modulus (%)
+      // - expontiation (**)
+      // - increment (++)
+      //       - pre   (++a)
+      //       - post (a++)
+      // - decrement (--)
+      //       - pre   (--a)
+      //       - post (a--)
+      // - expontiation example:
+      //           x=2,y=3
+      //           x**y output : 2*2*2=8
+}
+{
+
+  // 2) assignment operator 
+        //  - assign (=)
+        //  - addition and assign (+=)
+        //  - subtraction and assign (-=)
+        //  - multiplication and assign (*=)
+        //  - divison and assign (/=)
+        //  - expontiation and assign (**=)
+                
+         
+}
+{
+  // 3) comparision operator (relational)
+        // - equel to (=)
+        // - strict equel to (===)
+        // - not equel to (!=)
+        // -strict not  equel to (!==)
+        // -greater than (>)
+        // -greater than (>)
+        // -greater than (>)
+        // -lessthan (<)
+        // -greater or equel to (>=)
+        // -lessthan or equel to(<=)
+
+
+
+}
+{
+  // 4) ternary operator (conditional )
+       // "?"
+      // ":"
+}
+{
+  // 5) ḷogical operator 
+        // - &&  (logical end)
+        // - ||  (logical or)
+        // - !   (logical not)
+}
