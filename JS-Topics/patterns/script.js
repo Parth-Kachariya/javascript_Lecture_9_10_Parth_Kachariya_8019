650a7e28c2cf990f35cdda844ff43e4faf2767fa
parth-kachariya <parthkachariya668@gmail.com>
2025-10-13 17:04:55 +0530

    Update || 13-10

--- JS-Topics/patterns/script.js
+++ JS-Topics/patterns/script.js
@@ -1,5 +1,6 @@
 // patterns in js
 {
+  // 1.
   for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
       if (i == j) {
@@ -14,6 +15,7 @@
 document.write("<br>");
 
 {
+  // 2.
   for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
       if (i != j) {
@@ -28,6 +30,7 @@ document.write("<br>");
 document.write("<br>");
 
 {
+  // 3.
   for (let i = 0; i < 5; i++) {
     for (let j = 5; j > i; j--) {
       document.write(" * ");
@@ -38,6 +41,7 @@ document.write("<br>");
 document.write("<br>");
 
 {
+  // 4.
   for (let i = 5; i >= 0; i--) {
     for (let j = i; j < 5; j++) {
       document.write(" * ");
@@ -47,4 +51,43 @@ document.write("<br>");
 }
 document.write("<br>");
 
+// {
+//   // 5.
+//   for (let i = 0; i < 5; i++) {
+//     for (let j = 5; j > i; j--) {
+//       document.write(" * ");
+//     }
+//     document.write("<br>");
+//   }
+// }
+document.write("<br>");
 
+// {
+//   // 6.
+//   for (let i = 0; i < 5; i++) {
+//     for (let k = 0; k < i; k++) {
+//       document.write(" ");
+//     }
+//     for (let j = i; j < 5; j++) {
+//       document.write("* ");
+//     }
+//     document.write("<br>");
+//   }
+// }
+document.write("<br>");
+{
+  // 7.
+  for (let i = 0; i < 5; i++) {
+    for (let j = 5; j > i; j--) {
+      if ((i == 0 || i == 4) || (j == 0 || j == 4)) {
+        document.write(" * ");
+      }
+      else{
+        document.write(" ");
+
+      }
+    }
+    document.write("<br>");
+  }
+}
+document.write("<br>");
